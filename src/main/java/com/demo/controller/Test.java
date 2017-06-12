52ab161c4fac20e6bb7986760e2e4d81412eb161
Chen <1113973076@qq.com>
2017-06-12 20:58:53 +0800

    adjust address

--- src/main/java/com/demo/controller/Test.java
+++ src/main/java/com/demo/controller/Test.java
@@ -2,11 +2,13 @@ package com.demo.controller;
 
 import javax.annotation.Resource;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.demo.mapping.TestMapper;
+import com.demo.dao.TestMapper;
 
 @Controller
 
@@ -20,5 +22,11 @@ public class Test {
     public String test(){
         testMapper.selectByPrimaryKey(1);
         return testMapper.selectByPrimaryKey(1).getName();
+    }
+    public static void main(String[] args) {
+        PropertyConfigurator.configure( "C:/workspace/demo/src/main/resources/log4j.properties" );
+        Logger logger  =  Logger.getLogger(Test.class );
+        logger.debug( " debug " );
+        logger.error( " error " );
     }
 }
